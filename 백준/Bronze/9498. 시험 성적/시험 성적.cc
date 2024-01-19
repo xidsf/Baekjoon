0b0b18a9069b7201569aad4520918c819aefa517
xidsf <80446379+xidsf@users.noreply.github.com>
2024-01-19 17:00:29 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score;
+	cin >> score;
+	score = score / 10;
+	if (score >= 9) cout << "A";
+	else if (score >= 8) cout << "B";
+	else if (score >= 7) cout << "C";
+	else if (score >= 6) cout << "D";
+	else cout << "F";
+	return 0;
+}
