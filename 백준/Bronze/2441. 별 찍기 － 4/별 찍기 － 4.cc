cd27ce10b6efd9414554873a0e6fc0774b248283
xidsf <80446379+xidsf@users.noreply.github.com>
2024-03-16 16:43:40 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int cnt;
+	cin >> cnt;
+	for (int i = cnt; i > 0 ; i--)
+	{
+		for (int j = i; j < cnt; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << '*';
+		}
+		cout << "\n";
+	}
+
+    return 0;
+}
