63fc2adeceeac961a18d1a9df396f6418545cf54
xidsf <80446379+xidsf@users.noreply.github.com>
2024-04-01 22:47:56 +0900

    [Silver IV] Title: 스택, Time: 396 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+	int arr[10000];
+	int arrCount = 0;
+public:
+	void push(int value)
+	{
+		arr[arrCount] = value;
+		arrCount++;
+	}
+	int pop()
+	{
+		if (arrCount == 0) return -1;
+		else
+		{
+			arrCount--;
+			return arr[arrCount];
+		}
+	}
+	int size()
+	{
+		return arrCount;
+	}
+	int empty()
+	{
+		return (arrCount) == 0 ? 1 : 0;
+	}
+	int top()
+	{
+		if (arrCount == 0) return -1;
+		else
+		{
+			return arr[arrCount - 1];
+		}
+	}
+};
+
+int main()
+{
+	Stack stack;
+	int cnt;
+	string input;
+	int num;
+
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++)
+	{
+		cin >> input;
+		if (input == "push")
+		{
+			cin >> num;
+			stack.push(num);
+		}
+		else if (input == "pop")
+		{
+			cout << stack.pop() << "\n";
+		}
+		else if (input == "size")
+		{
+			cout << stack.size() << "\n";
+		}
+		else if (input == "empty")
+		{
+			cout << stack.empty() << "\n";
+		}
+		else if (input == "top")
+		{
+			cout << stack.top() << "\n";
+		}
+	}
+
+
+	return 0;
+}
