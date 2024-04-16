08b58ae0dcd058cad280146421dda2719c0e93c4
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-04-16 09:23:23 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -0,0 +1,49 @@
+#//바이러스
+#include <iostream>
+#include <stack>
+using namespace std;
+
+#define MAX 101
+int n, m;
+int cnt = 0;
+int arr[MAX][MAX];
+int visited[MAX] = { 0 };
+stack<int> s;
+
+void bfs(int v) {
+
+    s.push(1);
+    int current;
+    visited[1] = -1;
+    while (!s.empty())
+    {
+        current = s.top();
+        s.pop();
+        for (int i = 0; i < MAX; i++)
+        {
+            if (arr[current][i] == 1 && visited[i] != -1) 
+            {
+                s.push(i);
+                visited[i] = -1;
+                cnt++;
+            }
+        }
+    }
+}
+
+
+
+int main() {
+    cin >> n >> m;
+    for (auto i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        arr[x][y] = 1;
+        arr[y][x] = 1;
+    }
+    bfs(1);
+    cout << cnt;
+
+}
+
+
