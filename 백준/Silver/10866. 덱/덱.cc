25920a1bbc92acb768c4d544cf70a0105d349173
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-07-06 17:52:41 +0900

    [Silver IV] Title: 덱, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -1,179 +1,90 @@
 #include <iostream>
+#include <deque>
 using namespace std;
-
-struct Node {
-	int data;
-	Node* prev = nullptr;
-	Node* next = nullptr;
-};
-
-class Deque
-{
-private:
-	Node* head = nullptr;
-	Node* tail = nullptr;
-	int size = 0;
-public:
-	void push_front(int value)
-	{
-		Node* newNode = new Node;
-		newNode -> data = value;
-		if (size == 0)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else if (size == 1)
-		{
-			tail = head;
-			head = newNode;
-			tail->prev = head;
-			head->next = tail;
-		}
-		else
-		{
-			newNode->next = head;
-			head->prev = newNode;
-			head = newNode;
-		}
-		size++;
-	}
-	void push_back(int value)
-	{
-		Node* newNode = new Node;
-		newNode->data = value;
-		if (size == 0)
-		{
-			head = newNode;
-			tail = newNode;
-		}
-		else if (size == 1)
-		{
-			head = tail;
-			tail = newNode;
-			head->next = tail;
-			tail->prev = head;
-		}
-		else
-		{
-			newNode->prev = tail;
-			tail->next = newNode;
-			tail = newNode;
-		}
-		size++;
-	}
-	int pop_front()
-	{
-		if (size == 0) return -1;
-		int value = head->data;
-		Node* tempNode = head;
-		if (size == 1)
-		{
-			head = nullptr;
-			tail = nullptr;
-		}
-		else if(size == 2)
-		{
-			head = tail;
-			tail->prev = nullptr;
-		}
-		else
-		{
-			head = head->next;
-			head->prev = nullptr;
-		}
-		delete(tempNode);
-		size--;
-		return value;
-	}
-	int pop_back()
-	{
-		if (size == 0) return -1;
-		int value = tail->data;
-		Node* tempNode = tail;
-		if (size == 1)
-		{
-			head = nullptr;
-			tail = nullptr;
-		}
-		else if (size == 2)
-		{
-			tail = head;
-			head->next = nullptr;
-		}
-		else
-		{
-			tail = tail->prev;
-			tail->next = nullptr;
-		}
-		delete(tempNode);
-		size--;
-		return value;
-	}
-	int Size()
-	{
-		return size;
-	}
-	int empty()
-	{
-		return (size == 0) ? 1 : 0;
-	}
-	int front()
-	{
-		if (size > 0) return head->data;
-		else return -1;
-	}
-	int back()
-	{
-		if (size > 0) return tail->data;
-		else return -1;
-	}
-};
 
 
 int main()
 {
-	Deque deque;
-	int cnt, in;
-	string input;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	deque<int> d;
+
+	int cnt;
 	cin >> cnt;
+
+	string input;
+
 	for (int i = 0; i < cnt; i++)
 	{
 		cin >> input;
 		if (input == "push_front")
 		{
-			cin >> in;
-			deque.push_front(in);
+			int num;
+			cin >> num;
+			d.push_front(num);
 		}
-		if (input == "push_back")
+		else if (input == "push_back")
 		{
-			cin >> in;
-			deque.push_back(in);
+			int num;
+			cin >> num;
+			d.push_back(num);
 		}
 		else if (input == "pop_front")
 		{
-			cout << deque.pop_front() << "\n";
+			if (d.empty())
+			{
+				cout << -1 << "\n";
+				continue;
+			}
+			cout << d.front() << "\n";
+			d.pop_front();
 		}
 		else if (input == "pop_back")
 		{
-			cout << deque.pop_back() << "\n";
+			if (d.empty())
+			{
+				cout << -1 << "\n";
+				continue;
+			}
+			cout << d.back() << "\n";
+			d.pop_back();
 		}
 		else if (input == "size")
 		{
-			cout << deque.Size() << "\n";
+			cout << d.size() << "\n";
 		}
 		else if (input == "empty")
 		{
-			cout << deque.empty() << "\n";
+			if (d.empty())
+			{
+				cout << 1 << "\n";
+			}
+			else
+			{
+				cout << 0 << "\n";
+			}
 		}
 		else if (input == "front")
 		{
-			cout << deque.front() << "\n";
+			if (d.empty())
+			{
+				cout << -1 << "\n";
+				continue;
+			}
+			cout << d.front() << "\n";
 		}
 		else if (input == "back")
 		{
-			cout << deque.back() << "\n";
+			if (d.empty())
+			{
+				cout << -1 << "\n";
+				continue;
+			}
+			cout << d.back() << "\n";
 		}
 	}
+
 	return 0;
 }
