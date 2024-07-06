31fbabb9caa195e998d8687b20c2ca55a81f7195
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-07-06 20:10:15 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -2,42 +2,58 @@
 #include <vector>
 using namespace std;
 
+
 int main()
 {
-	vector<int> v1;
-	int cnt, input, line, max = 0;
-	long long sum = 0;
-	cin >> cnt >> line;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int cnt, reqCount;
+	cin >> cnt >> reqCount;
+
+	long long min = 1;
+	long long max = 0;
+
+	vector<int> v;
 	for (int i = 0; i < cnt; i++)
 	{
+		int input;
 		cin >> input;
-		v1.push_back(input);
-		max = max < input ? input : max;
+		v.push_back(input);
+		if (max < input)
+		{
+			max = input;
+		}
 	}
-	long long s = 1;
-	long long e = max;
-	long long m;
 
-	while (s <= e)
+	long long mid;
+	int ans;
+
+	while (min <= max)
 	{
-		sum = 0;
-		m = s + (e - s) / 2;
+		int lineCont = 0;
 
-		for (int i = 0; i < v1.size(); i++)
+		mid = min + (max - min) / 2;
+
+		for (int i = 0; i < v.size(); i++)
 		{
-			sum += v1[i] / m;
+			lineCont += v[i] / mid;
 		}
 
-		if (sum < line)
+		if (lineCont >= reqCount)
 		{
-			e = m - 1;
+			min = mid + 1;
 		}
-		else if (sum >= line)
+		else
 		{
-			s = m + 1;
+			max = mid - 1;
 		}
+		
+
 	}
-	cout << --s;
-	
+
+	cout << --min;
+
 	return 0;
 }
