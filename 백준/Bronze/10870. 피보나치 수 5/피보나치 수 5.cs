f764d5abe6825448f8acbaef008e93eb3e8938f1
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-07-18 19:23:25 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 64 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cs
@@ -0,0 +1,23 @@
+class Program
+{
+    static int fibonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        if (n == 1)
+        {
+            return 1;
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    static void Main()
+    {
+        int input = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(fibonacci(input));
+    }
+}
