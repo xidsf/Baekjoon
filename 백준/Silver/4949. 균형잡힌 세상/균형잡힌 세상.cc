0e219b68ae8e740c8145d0d6fc470d135f201daa
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-08-08 16:22:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 88 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -5,45 +5,59 @@ using namespace std;
 
 int main()
 {
-	stack<char> stack;
-	string s;
-	bool isCorrect;
-	
 	while (true)
 	{
-		isCorrect = true;
-		while (stack.size() > 0)
-		{
-			stack.pop();
-		}
-		getline(cin, s);
-		if (s == ".") break;
-		for (int i = 0; i < s.length(); i++)
-		{
-			if (!isCorrect) break;
-			switch (s[i])
-			{
-			case '[':
-				stack.push(']');
-				break;
-			case '(':
-				stack.push(')');
-				break;
-			case ']':
-				if (stack.size() > 0 && stack.top() == ']') stack.pop();
-				else isCorrect = false;
-				break;
-			case ')':
-				if (stack.size() > 0 && stack.top() == ')') stack.pop();
-				else isCorrect = false;
-				break;
-			default:
-				break;
+		string str;
+		getline(cin, str);
 
+		if (str == ".") break;
+
+		stack<char> s;
+		bool isNormal = true;
+
+		for (int i = 0; i < str.length() ; i++)
+		{
+			if (isNormal == false) break;
+			switch (str[i])
+			{
+				case '(':
+					s.push('(');
+					break;
+				case ')':
+					if (!s.empty() && s.top() == '(')
+					{
+						s.pop();
+					}
+					else
+					{
+						isNormal = false;
+					}
+					break;
+				case '[':
+					s.push('[');
+					break;
+				case ']':
+					if (!s.empty() && s.top() == '[')
+					{
+						s.pop();
+					}
+					else
+					{
+						isNormal = false;
+					}
+					break;
 			}
+
 		}
-		if (isCorrect && stack.size() == 0) cout << "yes\n";
-		else cout << "no\n";
+		if(s.empty() && isNormal == true)
+		{
+			cout << "yes\n";
+		}
+		else
+		{
+			cout << "no\n";
+		}
+		
 	}
 
 	return 0;
