eb482ab314bc6b7fbc1660976c1c532fb445513a
ChoiSeongJe <80446379+xidsf@users.noreply.github.com>
2024-08-10 21:51:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 628 ms, Memory: 8180 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -3,33 +3,79 @@
 #include <algorithm>
 using namespace std;
 
-int main()
+int length;
+vector<int> arr;
+vector<int> targets;
+
+int BS(int target)
 {
-	vector<int> v1;
-	vector<int> v2;
-	int input, temp;
-
-
-	cin >> input;
-	for (int i = 0; i < input; i++)
+	int start = 0;
+	int end = length - 1;
+	int minIdx = INT32_MAX;
+	while (start <= end)
 	{
-		cin >> temp;
-		v1.push_back(temp);
-	}
-	cin >> input;
+		int mid = (start + end) / 2;
 
-	for (int i = 0; i < input; i++)
-	{
-		cin >> temp;
-		v2.push_back(temp);
+		if (arr[mid] >= target)
+		{
+			if (arr[mid] == target) minIdx = min(minIdx, mid);
+			end = mid - 1;
+		}
+		else if (arr[mid] < target)
+		{
+			start = mid + 1;
+		}
 	}
 	
-	sort(v1.begin(), v1.end());
+	if (minIdx == INT32_MAX || arr[minIdx] != target) return 0;
 
-	for (int i = 0; i < v2.size(); i++)
+	start = minIdx;
+	end = length - 1;
+	int maxIdx = length;
+
+	while (start <= end)
 	{
-		temp = upper_bound(v1.begin(), v1.end(), v2[i]) - lower_bound(v1.begin(), v1.end(), v2[i]);
-		cout << temp << " ";
+		int mid = (start + end) / 2;
+
+		if (arr[mid] > target)
+		{
+			maxIdx = min(maxIdx, mid);
+			end = mid - 1;
+		}
+		else if (arr[mid] <= target)
+		{
+			start = mid + 1;
+		}
+	}
+
+	return maxIdx - minIdx;
+}
+
+int main()
+{
+	cin >> length;
+
+	for (int i = 0; i < length; i++)
+	{
+		int temp;
+		cin >> temp;
+		arr.push_back(temp);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	int cnt;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++)
+	{
+		int temp;
+		cin >> temp;
+		targets.push_back(temp);
+	}
+
+	for (int i = 0; i < targets.size(); i++)
+	{
+		cout << BS(targets[i]) << "\n";
 	}
 
 	return 0;
